d9669d9b189f14b6c9df3ade096a483b3c065aa0
slehaff <samir@lehaff.dk>
2018-11-30 14:24:02 +0100

    update

--- djback/train/models.py
+++ djback/train/models.py
@@ -3,11 +3,18 @@ from django.db import models
 
 
 class TrainingRepo(models.Model):
-    name = models.CharField(max_legth=30)
+    "Training data repository"
+    name = models.CharField(max_length=30)
     started = models.DateField
 
 
 class TrainingData(models.Model):
+    "Training data set comprising train, test and validate data"
     repo = models.ForeignKey(TrainingRepo, on_delete=models.CASCADE)
     width = models.PositiveIntegerField
     height = models.PositiveIntegerField
+
+
+class ImageFolder(models.Model):
+    "Single image folder containing input and ground truth image"
+    train_data = models.ForeignKey(TrainingData, on_delete=models.CASCADE)
