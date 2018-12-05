d158d3883b2c9d151f2297779fbbb36837248040
slehaff <samir@lehaff.dk>
2018-12-05 12:00:22 +0100

    update

--- djback/train/views.py
+++ djback/train/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 
 
 def collect_data(request):
+    'start threads, send message to rpi3'
     print('collect data!')
     return HttpResponse('train view ...')
 
